8b87ccb4a7a5d646f47c4e267237e7ec3efc7758
Mandar Ingle <mandar.ingle@spadeworx.com>
2022-02-12 15:28:36 +0530

    Add project files.

---
+++ ReadCsv/Program.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using System.IO;
+
+namespace ReadCsv
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConvertCSVtoDataTable("strFilePath");
+        }
+        public static DataTable ConvertCSVtoDataTable(string strFilePath)
+        {
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(@"C:\temp\username.csv"))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+    }
+}
